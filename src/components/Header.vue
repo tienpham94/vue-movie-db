44c03a4c8035a0437c35f8810958486c4f1569b2
Tien Pham <r3dsky02@yahoo.com.vn>
2018-10-13 20:31:00 +0300

    Animation in Vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,10 @@
 <template>
   <header>
     <h1><router-link to="/" >{{ title }}</router-link></h1>
+    <transition name="fade">
+      <h1 v-if="show">Animated</h1>
+    </transition>
+    <button @click="show = !show">Show/Hide</button>
   </header>
 </template>
 
@@ -12,6 +16,7 @@ export default {
   },
   data() {
     return {
+      show: false,
       name: 'Scott'
     };
   }
@@ -27,4 +32,19 @@ header {
 h1 {
   margin: 0;
 }
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.3s ease;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+  transform: scale(0);
+}
+
+/* name-enter -> name-enter-to */
+/* name-enter-active */
+
+/* name-leave -> name-leave-to */
+/* name-leave-active */
 </style>
